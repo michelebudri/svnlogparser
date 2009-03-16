764c0b064c9e218e55c78045469b6aeafebfd533
jeroen.v.schagen <jeroen.v.schagen@f7f86b48-1219-11de-af30-3d3b2b6d83b0>
2009-03-16 15:12:43 +0000

    kut parser

--- SVNLogParser/src/main/java/org/qwerzor/svnparser/Main.java
+++ SVNLogParser/src/main/java/org/qwerzor/svnparser/Main.java
@@ -4,7 +4,10 @@ import java.io.IOException;
 
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.qwerzor.svnparser.model.Log;
 import org.qwerzor.svnparser.xml.DocumentParser;
+import org.qwerzor.svnparser.xml.LogParser;
+import org.qwerzor.svnparser.xml.ParsingException;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
@@ -18,12 +21,15 @@ public class Main {
 	public static void main(String[] args) {
 		try {
 			Document document = DocumentParser.parseDocument(TEST_LOG);
-			System.out.println(document.toString());
+			Log log = LogParser.parseLog(document);
+			System.out.println("LOLS " + log.toString());
 		} catch (SAXException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (ParsingException e) {
 			e.printStackTrace();
 		}
 	}
--- SVNLogParser/src/main/java/org/qwerzor/svnparser/xml/LogParser.java
+++ SVNLogParser/src/main/java/org/qwerzor/svnparser/xml/LogParser.java
@@ -1,7 +1,14 @@
 package org.qwerzor.svnparser.xml;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 import org.qwerzor.svnparser.model.Log;
+import org.qwerzor.svnparser.model.LogEntry;
+import org.qwerzor.svnparser.model.Path;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
 /**
  * Parse XML document object into a specific Log model.
@@ -10,16 +17,74 @@ import org.w3c.dom.Document;
  * @date 16-03-2009
  */
 public class LogParser {
+	
+	public static final String ENTRY_ID = "logentry";
+	
+	public static final String REVISION_ID = "revision";
+	public static final String AUTHOR_ID = "author";
+	public static final String DATE_ID = "date";
+	public static final String PATHS_ID = "paths";
+	public static final String MESSAGE_ID = "msg";	
+	
+	public static final String PATH_ID = "path";
+	public static final String ACTION_ID = "action";
 
-	public Log parseLog(Document document) {
+	public static Log parseLog(Document document) throws ParsingException {
 		Log log = new Log();
 		
 		if(document == null) { return log; }
 		
 		// Attach entries
-		
+		ArrayList<LogEntry> entries = new ArrayList<LogEntry>();
+		NodeList elements = document.getElementsByTagName(ENTRY_ID);
+		for(int i = 0; i < elements.getLength(); i++) {
+			entries.add(parseEntry((Element) elements.item(i)));
+		}
 		
 		return log;
+	}
+	
+	@SuppressWarnings("deprecation")
+	public static LogEntry parseEntry(Element element) throws ParsingException {
+		LogEntry entry = new LogEntry();
+		
+		// Retrieve revision number
+		String revision = "";
+		try {
+			revision = element.getAttribute(REVISION_ID);
+			entry.setRevision(Integer.parseInt(revision));
+		} catch(NumberFormatException e) {
+			throw new ParsingException(revision + " is not a valid revision number");
+		}
+		
+		// Retrieve author
+		String author = getTextValue(element, AUTHOR_ID);
+		entry.setAuthor(author);
+		
+		// Retrieve date
+		String date = getTextValue(element, DATE_ID);
+		try {
+			date.replace("T", null);
+			date.replace("Z", null);
+			System.out.println(date);
+			//entry.setDate(new Date(Date.parse(date)));
+		} catch(Exception e) {
+			throw new ParsingException(date + " is not a valid date");
+		}
+		
+		return entry;
+	}
+	
+	public static Path parsePath(Element element) throws ParsingException {
+		Path path = new Path();
+		
+		System.out.println(element.toString());
+		
+		return path;
+	}
+	
+	public static String getTextValue(Element element, String identifier) {
+		return element.getElementsByTagName(identifier).item(0).getNodeValue();
 	}
 	
 }
